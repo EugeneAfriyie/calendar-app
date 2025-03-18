3ee408245a6b9c5a0835b542985b0c2b6894e68c
Eugene Afriyie <groupeight00@gmail.com>
2025-03-18 22:39:23 +0000

    importing dependencies

--- src/Components/Calender_app.jsx
+++ src/Components/Calender_app.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import './Calendar.css'
 
 const Calender_app = () => {
   return (
-    <div className='calendar_app'>
-      
+    <div className='calendar'>
+      <h1 className="heading">Calendar</h1>
+      <div className="navigate-date">
+        <h2 className="month">May,</h2>
+        <h2 className="year">2025</h2>
+        <div className="buttons">
+          <i className="bx bx-chevron-left icon"></i>
+          <i className="bx bx-chevron-right"></i>
+        </div>
+      </div>
     </div>
   )
 }
