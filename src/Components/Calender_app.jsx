95d86de04d5a669848170e9388dadf55377a023c
Eugene Afriyie <groupeight00@gmail.com>
2025-03-23 23:54:22 +0000

    rendering the months dynamically

--- src/Components/Calender_app.jsx
+++ src/Components/Calender_app.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Calendar.css'
 
 const Calender_app = () => {
 
+  const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
+  const monthsOfYear = 
+  [
+     'January',
+     'February',
+     'March',
+     'April',
+     'May',
+      'June',
+      'July',
+      'August',
+      'October',
+      'November',
+      'December' 
+    ]
+
+
+    const currentDate = new Date();
+    const currentDay = currentDate.getDate();
+
+
+
+    const [currentMonth, setCurrentMonth] = useState(currentDate.getMonth());
+    const [currentYear, setCurrentYear] = useState(currentDate.getFullYear());
+    const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+    const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+
+    console.log(firstDayOfMonth, daysInMonth)
 
 
 
 
 
 
-  
+
   return (
    <div className="calendar-app">
      <div className='calendar'>
@@ -24,46 +53,22 @@ const Calender_app = () => {
         </div>
       </div>
       <div className="weekdays">
-        <span>Sun</span>
-        <span>Mon</span>
-        <span>Tues</span>
-        <span>Wed</span>
-        <span>Thur</span>
-        <span>Fri</span>
-        <span>Sat</span>
+       { 
+        daysOfWeek.map(day =>(  <span key={day}>{day}</span> ))}
+    
       </div>
       <div className="days">
-  <span>1</span>
-  <span>2</span>
-  <span>3</span>
-  <span>4</span>
-  <span>5</span>
-  <span>6</span>
-  <span>7</span>
-  <span>8</span>
-  <span>9</span>
-  <span>10</span>
-  <span>11</span>
-  <span>12</span>
-  <span>13</span>
-  <span>14</span>
-  <span className='current-day'>15</span>
-  <span>16</span>
-  <span>17</span>
-  <span>18</span>
-  <span>19</span>
-  <span>20</span>
-  <span>21</span>
-  <span>22</span>
-  <span>23</span>
-  <span>24</span>
-  <span>25</span>
-  <span>26</span>
-  <span>27</span>
-  <span>28</span>
-  <span>29</span>
-  <span>30</span>
-  <span>31</span>
+        {...Array(firstDayOfMonth)
+        .fill(null)
+        .map((_, index) => (
+          <div key={index} className="day"></div>
+        ))}
+        {Array(daysInMonth)
+        .fill(null)
+        .map((_, index) => (
+          <span key={index} className="day">{index + 1}</span>
+        ))}
+
 </div>
 
     </div>
